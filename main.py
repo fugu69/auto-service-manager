f431011036e3434137c047fec97f76ca490f0495
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-20 15:28:30 +0700

    Add flash messages

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
 from forms import RegisterForm, LoginForm
 
 load_dotenv()
@@ -13,9 +13,12 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}", "success")
+        return redirect(url_for("index"))
     return render_template("register.html", title="Register", form=form)
 
 @app.route("/login")
