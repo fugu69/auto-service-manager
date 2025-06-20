eab951f3ac4405ffd5bba71563d2014528698865
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-20 13:34:31 +0700

    Add Login and Register forms to routes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect
+from forms import RegisterForm, LoginForm
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route("/")
 @app.route("/home")
@@ -9,11 +15,13 @@ def index():
 
 @app.route("/register")
 def register():
-    return render_template("register.html", title="Register")
+    form = RegisterForm()
+    return render_template("register.html", title="Register", form=form)
 
 @app.route("/login")
 def login():
-    return render_template("login.html", title="Log In")
+    form = LoginForm()
+    return render_template("login.html", title="Log In", form=form)
 
 
 
